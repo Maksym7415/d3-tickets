9681e8c4b8142f330981b3b84a42523fb963ae40
Maksym7415 <popovmaksim7415@gmail.com>
2021-12-15 14:20:09 +0200

    added data from new json

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jsonfile = require('jsonfile')
 
 const app = express();
 const httpServer = http.createServer(app);
-const data = require('./data/data_full.js');
+const data = require('./data/Drawing2.json');
 
 app.use(cors())
 app.use('/', express.static(path.join(__dirname, './public')));
@@ -16,22 +16,43 @@ app.get('/api', (req, res) => {
   res.send('hello')
 })
 
-const arr = data.geometries
-  .map((item) => item.data.attributes.position.array.toString().split(',0,'))
-  .slice(0, 3)
-  // .reduce((acc, item) => ([...acc, ...item]),[])
-  .map((item) => {
-    return {room: item.map((el) => ({x: Number(el.split(',')[0]), y: Number(el.split(',')[1])})), data: []}
-  })
+const result = {};
 
-  data.geometries
-  .map((item) => item.data.attributes.position.array.toString().split(',0,'))
-  .slice(3, 45)
-  .forEach((item) => {
-    arr[0].data.push(item.map((el) => ({x: Number(el.split(',')[0]), y: Number(el.split(',')[1])})))
-  })
+data.object.children[0].children.forEach((object) => {
+  if (object.name.includes('Sector')) {
+    const geometry = data.geometries.find((geo) => geo.uuid === object.children[0].children[0].geometry)
+    const coord = geometry.data.attributes.position.array.toString().split(',0,')
+      .map((el) => ({x: Number(el.split(',')[0]), y: Number(el.split(',')[1])}))
+    
+    if (!result[object.name]) {
+      result[object.name] = {
+        room: coord
+      }
+    } else result[object.name].room = coord
+    
+  }
+  if (object.name.includes('Seat')) {
+    const sector = `Sector-${object.name.split('-')[1]}`;
+    console.log(object.children[0].children[0].geometry)
+    object.children.forEach((seat) => {
+      const geometry = data.geometries.find((geo) => geo.uuid === seat.children[0].geometry);
+      const coord = geometry.data.attributes.position.array.toString().split(',0,')
+        .map((el) => ({x: Number(el.split(',')[0]), y: Number(el.split(',')[1])}))
+      
+      if (!result[sector]) {
+        result[sector] = {
+          seats: [coord]
+        }
+      } else if (result[sector] && !result[sector].seats) {
+        result[sector].seats = [coord]
+      } else {
+        result[sector].seats.push(coord)
+      }
+    })
+  }
+})
 
-jsonfile.writeFile('./public/test4.json', arr, function (err) {
+jsonfile.writeFile('./public/test4.json', Object.values(result), function (err) {
   if (err) console.error(err)
 })
 
